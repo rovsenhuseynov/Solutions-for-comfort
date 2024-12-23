45553f4576fa0887984695f7f1d3b925ff214034
rovshen <rovsen.dev@gmail.com>
2024-12-23 17:03:19 +0400

    коррекция - ExpandablePanelSlider

--- src/pages/Home/ExpandablePanelS/panelsData.js
+++ src/pages/Home/ExpandablePanelS/panelsData.js
@@ -9,30 +9,37 @@ import ImageSliderPanel_7 from "../../../assets/img/main_banner_bg/Wooden_press_
 const panelsData = [
   {
     title: "Paltarqurutma stendi",
+    description: "Ən sevilən qurutma quruluşu" ,
     imageUrl: ImageSliderPanel_1,
   },
   {
     title: "Friendly coffee evening",
+    description: "" ,
     imageUrl: ImageSliderPanel_2,
   },
   {
     title: "Metal soba",
+    description: "Havanı deyil, qablarınızı maksimum  qızdırın!" ,
     imageUrl: ImageSliderPanel_3,
   },
   {
     title: "Paltarqurutma stendi",
+    description: "Divara quraşdırılan paslanmayan poladdan paltar qurutma qurğusu",
     imageUrl: ImageSliderPanel_4,
   },
   {
     title: "Paltarqurutma stendi",
+    description: "Ən sevilən qurutma quruluşu",
     imageUrl: ImageSliderPanel_5,
   },
   {
     title: "Taxta pres",
+    description: "Xəngəl xəmiri rahatca yaymaq üçün taxta pres" ,
     imageUrl: ImageSliderPanel_6,
   },
   {
     title: "Tərtibatlı taxta təqvim",
+    description: "Fərqli tərtibatla hazırlanmış taxta təqvim" ,
     imageUrl: ImageSliderPanel_7,
   },
 ];
