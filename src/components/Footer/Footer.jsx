d05d2be77fe483512061d8b98ae6d02f691706b0
rovshen <rovsen.dev@gmail.com>
2024-12-25 14:48:53 +0400

    Работа на - .footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./Footer.scss";
 import Logo from "../../reusableСomponents/Logo/Logo";
 import NavList from "../../reusableСomponents/NavList/NavList";
+import socialWhatsApp from "../../assets/img/svg/whatsapp-svg.svg";
+import socialtel from "../../assets/img/svg/phone_blue.svg";
+import externalLinks from "../../assets/img/svg/icon_external_link_symbol.svg";
 
 const Footer = () => {
   return (
@@ -16,8 +19,37 @@ const Footer = () => {
         </div>
 
         <div className="footer__top-contact">
-          <p>tel: 050 6406701</p>
-          <p>tel: 050 6406701</p>
+
+          <div className="footer__top-tel-img_block">
+            <img
+              className="footer__top-tel-img"
+              src={socialtel}
+              alt="socialWhatsApp"
+            />
+            <p className="footer__top-tel-num"> 050 6406701</p>
+          </div>
+
+          <div className="footer__top-WhatsApp-img_block">
+            <img
+              className="footer__top-WhatsApp-img"
+              src={socialWhatsApp}
+              alt="socialWhatsApp"
+            />
+            <p className="footer__top-WhatsApp-num"> 050 6406701</p>
+          </div>
+
+
+          <div className="footer__top-externalLinks-img_block">
+            <img
+              className="footer__top-externalLinks-img"
+              src={externalLinks}
+              alt="socialWhatsApp"
+            />
+            <p className="footer__top-externalLinks-num">solutions-for-comfort.vercel.app</p>
+          </div>
+
+
+
         </div>
       </div>
 
