21bf744388b4336cb2e53a0d781d566edbfe00ba
rovshen <rovsen.dev@gmail.com>
2024-12-30 21:26:14 +0400

    коррекция проблемной анимации в DryingRack.scss - .containers   из за того что старая тормозила бегущую дорожку в header-e

--- src/pages/Products/DryingRack/DryingRack.jsx
+++ src/pages/Products/DryingRack/DryingRack.jsx
@@ -1,67 +1,3 @@
-// import React, { useState } from "react";
-// import sliderData from "./SliderData";
-// import "./DryingRack.scss";
-
-// const DryingRack = () => {
-//   const [activeSlide, setActiveSlide] = useState(1);
-
-//   const handleNext = (current) => {
-//     setActiveSlide(current === sliderData.length ? 1 : current + 1);
-//   };
-
-//   const handlePrev = (current) => {
-//     setActiveSlide(current === 1 ? sliderData.length : current - 1);
-//   };
-
-//   const handleDotClick = (index) => {
-//     setActiveSlide(index + 1);
-//   };
-
-//   return (
-//     <div className="containers">
-//       {sliderData.map((slide, index) => (
-//         <input
-//           key={`input-${index}`}
-//           type="radio"
-//           id={`i${index + 1}`}
-//           name="images"
-//           checked={activeSlide === index + 1}
-//           readOnly
-//         />
-//       ))}
-
-//       {sliderData.map((slide, index) => (
-//         <div className="slide_img" id={slide.id} key={slide.id}>
-//           <img src={slide.src} alt={slide.alt} />
-//           <label className="prev" onClick={() => handlePrev(activeSlide)}>
-//             <span>&#x2039;</span>
-//           </label>
-//           <label className="next" onClick={() => handleNext(activeSlide)}>
-//             <span>&#x203a;</span>
-//           </label>
-//         </div>
-//       ))}
-
-//       <div id="nav_slide">
-//         {sliderData.map((_, index) => (
-//           <label
-//             key={`dot-${index}`}
-//             className="dots"
-//             id={`dot${index + 1}`}
-//             onClick={() => handleDotClick(index)}
-//           ></label>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DryingRack;
-
-
-
-
-
 import React, { useState } from "react";
 import sliderData from "./SliderData";
 import "./DryingRack.scss";
@@ -84,7 +20,6 @@ const DryingRack = () => {
   return (
     <div className="containers">
       {sliderData.map((slide, index) => (
-        
         <input
           key={`input-${index}`}
           type="radio"
@@ -95,7 +30,6 @@ const DryingRack = () => {
         />
       ))}
 
-
       {sliderData.map((slide, index) => (
         <div
           className={`slide_img ${activeSlide === index + 1 ? "active" : ""}`}
@@ -103,7 +37,9 @@ const DryingRack = () => {
           key={slide.id}
         >
           <img src={slide.src} alt={slide.alt} />
-          <div className="slide_context">{activeSlide === index + 1 && slide.context}</div>
+          <div className="slide_context">
+            {activeSlide === index + 1 && slide.context}
+          </div>
           <label className="prev" onClick={() => handlePrev(activeSlide)}>
             <span>&#x2039;</span>
           </label>
@@ -128,3 +64,5 @@ const DryingRack = () => {
 };
 
 export default DryingRack;
+
+
---
+++ src/reusableСomponents/ImageSlider/ImageSlider.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./ImageSlider.scss"
+
+const ImageSlider = () => {
+  return (
+    <div>ImageSlider</div>
+  )
+}
+
+export default ImageSlider
