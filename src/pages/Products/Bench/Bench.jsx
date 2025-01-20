d4a037a874a02706e258960d07259b4c4df4d891
rovshen <rovsen.dev@gmail.com>
2025-01-20 22:18:08 +0400

    коррекция - Bench

--- src/pages/Products/Bench/Bench.jsx
+++ src/pages/Products/Bench/Bench.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import "./Bench.scss"
-import Slider from '../../../reusableСomponents/ImageSlider/Slider'
-import BenchData from './Bench'
+import React from "react";
+import "./Bench.scss";
+import Slider from "../../../reusableСomponents/ImageSlider/Slider";
+import BenchData from "./Bench";
 
 const Bench = () => {
   return (
-    <section className='bench-section container'>
-        <Slider
-        sliderData={BenchData}
-        contextClass="drying-rack-context"
-      />
+    <section className="bench-section container">
+      <Slider sliderData={BenchData} contextClass="drying-rack-context" />
     </section>
-  )
-}
+  );
+};
 
-export default Bench
+export default Bench;
